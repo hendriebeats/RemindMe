a3c8a79ec2595e68651e1134e8ec3f6d1c0ac9e9
slicknick96 <nickhoulihan96@hotmail.com>
2017-04-27 10:45:52 -0400

    version 11.1
    added date, and time buttons within update
    #SlickNick

--- app/src/main/java/com/example/hendriebeats/remindme/UpdateTaskActivity.java
+++ app/src/main/java/com/example/hendriebeats/remindme/UpdateTaskActivity.java
@@ -1,20 +1,29 @@
 package com.example.hendriebeats.remindme;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class UpdateTaskActivity extends AppCompatActivity {
 
     EditText title, description, dateTxt, timeTxt;
     TextView PlaceTitleTxt, PlaceAddressTxt;
-    Button changeLocationBtn, updateBtn;
+    Button changeLocationBtn, updateBtn,chooseDateBtn, chooseTimeBtn;
     String currentTaskId, placeTitle, placeAddress, placeLatitude, placeLongitude, placeLocale, isUpdated, currentUserId;
+    Calendar myCalendar;
+    DatePickerDialog.OnDateSetListener dateDialog;
     public DatabaseHandler db;
 
 
@@ -25,13 +34,17 @@ public class UpdateTaskActivity extends AppCompatActivity {
 
         db = new DatabaseHandler(this);
 
-        //Get Current Task ID from Previous Activity
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
+            /**
+             * Get Current Task ID from Previous Activity
+             */
             currentTaskId = extras.getString("currentTaskId");
             currentUserId = extras.getString("currentUserId");
 
-            //Get Location Elements
+            /**
+             * Get Location Elements
+             */
             placeTitle = extras.getString("placeTitle");
             placeAddress = extras.getString("placeAddress");
             placeLatitude = extras.getString("placeLatitude");
@@ -44,22 +57,26 @@ public class UpdateTaskActivity extends AppCompatActivity {
         description = (EditText) findViewById(R.id.descriptionTxt);
         dateTxt = (EditText) findViewById(R.id.dateTxt);
         timeTxt = (EditText) findViewById(R.id.timeTxt);
-        changeLocationBtn = (Button) findViewById(R.id.changeLocationBtn);
+        changeLocationBtn = (Button) findViewById(R.id.locationBtn);
         updateBtn = (Button) findViewById(R.id.updateBtn);
+        chooseDateBtn = (Button) findViewById(R.id.chooseDateBtn);
+        chooseTimeBtn = (Button) findViewById(R.id.chooseTimeBtn);
         PlaceTitleTxt = (TextView) findViewById(R.id.placeTitleTxt);
         PlaceAddressTxt = (TextView) findViewById(R.id.placeAddressTxt);
 
-
-        //Set all the displayed fields equal to the current Task's values
+        /**
+         * Set all the displayed fields equal to the current Task's values
+         */
         title.setText(db.getTaskById(Integer.parseInt(currentTaskId)).getTitle());
         description.setText(db.getTaskById(Integer.parseInt(currentTaskId)).getDescription());
         dateTxt.setText(db.getTaskById(Integer.parseInt(currentTaskId)).getDate());
         timeTxt.setText(db.getTaskById(Integer.parseInt(currentTaskId)).getTime());
-
         PlaceTitleTxt.setText(db.getPlaceById(db.getTaskById(Integer.parseInt(currentTaskId)).getPlaceId()).getTitle());
         PlaceAddressTxt.setText(db.getPlaceById(db.getTaskById(Integer.parseInt(currentTaskId)).getPlaceId()).getAddress());
 
-        //Update Location field when updated via Place Picker
+        /**
+         * Update Location field when updated via Place Picker
+         */
         try{
             if(isUpdated.equals("yes")){
                 PlaceTitleTxt.setText(placeTitle);
@@ -67,9 +84,9 @@ public class UpdateTaskActivity extends AppCompatActivity {
             }
         } catch(Exception e) {}
 
-
-
-        //On Click Listener
+        /**
+         * Location Onclick Listener to update the location from Place Picker
+         */
         changeLocationBtn.setOnClickListener(
                 new View.OnClickListener(){public void onClick(View view) {
                     Intent i = new Intent(UpdateTaskActivity.this, PlacePickerUpdateTaskActivity.class);
@@ -78,7 +95,55 @@ public class UpdateTaskActivity extends AppCompatActivity {
                     startActivity(i);
                 }});
 
-        //On Click Listener
+        /**
+         * Create Time Picker
+         */
+        chooseTimeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(UpdateTaskActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        timeTxt.setText(selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//Yes 24 hour time
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+            }
+        });
+
+        /**
+         * Create Date Picker
+         */
+        myCalendar = Calendar.getInstance();
+        dateDialog = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDate();
+            }
+
+        };
+
+        /**
+         * Date Onclick Listener to update the date form Date Picker
+         */
+        chooseDateBtn.setOnClickListener(
+                new View.OnClickListener(){public void onClick(View view) {
+                    chooseDate();
+                }});
+
+        /**
+         * Update Onclick Listener to update the task list with the new information provided by user
+         */
         updateBtn.setOnClickListener(
                 new View.OnClickListener(){public void onClick(View view) {
                     //create task object
@@ -108,5 +173,17 @@ public class UpdateTaskActivity extends AppCompatActivity {
                     i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(i);
                 }});
+    }
+    public void chooseDate(){
+        new DatePickerDialog(UpdateTaskActivity.this, dateDialog, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+    }
+
+    private void updateDate() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dateTxt.setText(sdf.format(myCalendar.getTime()));
     }
 }
